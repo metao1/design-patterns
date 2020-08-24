50fd880e23a4e4202fe7b645bf40b5d146fab86f
mehrdadkarami <mehrdad.allahkarami@syncier.com>
2020-08-24 21:28:17 +0200

    added  mediator

---
+++ src/main/java/com/metao/dp/iterator/Container.java
@@ -0,0 +1,5 @@
+package com.metao.dp.iterator;
+
+public interface Container {
+    public Iterator getIterator();
+}
---
+++ src/main/java/com/metao/dp/iterator/Iterator.java
@@ -0,0 +1,7 @@
+package com.metao.dp.iterator;
+
+public interface Iterator {
+    public boolean hasNext();
+
+    public Object next();
+}
