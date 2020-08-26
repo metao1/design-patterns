4c38d36be9cae3c9bae89e433f8a6be71745e72a
mehrdadkarami <mehrdad.allahkarami@syncier.com>
2020-08-26 11:41:38 +0200

    ami

--- src/main/java/com/metao/dp/ami/App.java
+++ src/main/java/com/metao/dp/ami/App.java
@@ -9,8 +9,13 @@ public class App {
         var executor = new ThreadAsyncExecutor();
 
         var stringAsyncResult1 = executor.startProcess(call("10"));
+        System.out.println("runner 1");
         var stringAsyncResult2 = executor.startProcess(call("20"));
+        System.out.println("runner 2");
         var stringAsyncResult3 = executor.startProcess(call("30"));
+        System.out.println("runner 3");
+        var stringAsyncResult4 = executor.startProcess(call("50"));
+        System.out.println("runner 4");
 
         var integerAsyncResult1 = executor.startProcess(call(40), callback("Callback result 1"));
 
